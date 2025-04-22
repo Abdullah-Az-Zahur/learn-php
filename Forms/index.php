3d97e5a86305ff4efe0a91315f3fb848c30b4207
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-04-22 11:41:20 +0600

    forms & superglobals

--- Forms/index.php
+++ Forms/index.php
@@ -1,8 +1,17 @@
 <?php
-echo "<pre>";
-var_dump($_SERVER);
-echo "</pre>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $phone = filter_input(INPUT_POST, "phone", FILTER_SANITIZE_NUMBER_INT);
+
+    if ($name && $email && $phone) {
+        echo "contact Added : $name ($email , $phone)";
+    } else {
+        echo "Invalid Input";
+    }
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,15 +23,18 @@ echo "</pre>";
 </head>
 
 <body>
-    <form action="" method="get">
+    <form action="" method="post" enctype="multipart/form-data">
         <label for="name">Name:</label>
-        <input type="text" name="name" id="" required>
+        <input type="text" name="name" id="">
 
         <label for="email">Email</label>
         <input type="text" name="email" id="">
 
         <label for="phone">Phone</label>
         <input type="tel" name="phone" id="">
+
+        <label for="image">Image</label>
+        <input type="file" name="image" id="" accept="image/*">
 
         <button type="submit">Add Contact</button>
     </form>
