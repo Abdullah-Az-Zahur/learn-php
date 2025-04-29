6704f0d389e6340481efeae1de53d67030d6b723
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-04-29 12:18:18 +0600

    Build a calculator

---
+++ MyWebsite-Calculator/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/reset.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <input type="number" name="num01" placeholder="First Number">
+        <select name="operator">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="number" name="num02" placeholder="Second Number">
+        <button type="submit">Calculate</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // get data from inputs
+        $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
+        $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+        $operator = htmlspecialchars($_POST["operator"]);
+
+        // Error Handlers
+        $errors = false;
+
+        if (empty($num01) || empty($num02) || empty($operator)) {
+            echo "<p>Fill in all field</p>";
+            $errors = true;
+        }
+
+        if (!is_numeric($num01) || !is_numeric($num02)) {
+            echo "<P>Only numbers</P>";
+            $errors = true;
+        }
+
+        // Calculate if no errors
+        if (!$errors) {
+            $value = 0;
+            switch ($operator) {
+                case 'add':
+                    $value = $num01 + $num02;
+                    break;
+                case 'subtract':
+                    $value = $num01 - $num02;
+                    break;
+                case 'multiply':
+                    $value = $num01 * $num02;
+                    break;
+                case 'divide':
+                    $value = $num01 / $num02;
+                    break;
+                default:
+                    echo "<p>Something Wrong!</p>";
+            }
+
+            echo "<p>Result = " . $value . "</p>";
+        }
+    }
+    ?>
+
+</body>
+
+</html>
