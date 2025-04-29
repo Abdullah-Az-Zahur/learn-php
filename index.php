987785b5b0ca1537fdb864a542301cbf6d48d513
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-04-29 09:24:35 +0600

    control stractures in PHP

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 <body>
     <main>
-        
+
         <!-- The Codeholic -->
         <!-- <div>
             <?php
@@ -103,9 +103,10 @@
         </div> -->
 
 
-            
+
         <!-- Dani Krossing -->
-        <div>
+        <!-- operators -->
+        <!-- <div>
             <?php
             // $a = "Hello";
             // $b = "World";
@@ -131,6 +132,47 @@
             echo "<br>";
             echo $b;
 
+            ?>
+        </div> -->
+
+        <!-- Loop -->
+        <div>
+            <?php
+
+            $bool = true;
+            $a = 1;
+            $b = 4;
+
+            $result = match ($a) {
+                1, 3, 5 => "variable a is equal to one!",
+                2 => "variable a is equal to two!",
+            };
+
+            echo $result;
+
+            echo "<br>";
+
+            switch ($a) {
+                case 1:
+                    echo "The first case correct";
+                    break;
+                case 3:
+                    echo "The second case correct";
+                    break;
+
+                default:
+                    echo "None of condition were true";
+            }
+
+            echo "<br>";
+
+            if ($a < $b && !$bool) {
+                echo "First Condition is true!";
+            } elseif ($a < $b && $bool) {
+                echo "Second Condition is true!";
+            } else {
+                echo "None of condition were true";
+            }
             ?>
         </div>
     </main>
