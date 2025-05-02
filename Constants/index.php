708d54dfbc2ab29b6bf112e1b49e51027278d7a1
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-02 08:14:05 +0600

    Constans in PHP

---
+++ Constants/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $name = "Gias";
+    define("PI", 3.1416);
+    echo PI;
+    echo "<br>";
+
+    function test()
+    {
+        echo PI;
+    }
+    test();
+    ?>
+</body>
+
+</html>
