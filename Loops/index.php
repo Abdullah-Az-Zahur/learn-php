4b84e5224ea8909db258e3dedbdb20fece168fad
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-02 20:53:25 +0600

    Loops in Php

---
+++ Loops/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    for ($i = 0; $i <= 10; $i++) {
+        // echo "This is iteration number " . $i . "<br>";
+        echo "This is iteration number $i <br>";
+    }
+
+    $test = 5;
+    while ($test < 10) {
+        echo "$test <br>";
+        $test++;
+    }
+
+    $fruits = ["Apple", "Banana", "Orange"];
+
+    foreach ($fruits as $fruit) {
+        echo "$fruit <br>";
+    }
+
+    ?>
+</body>
+
+</html>
