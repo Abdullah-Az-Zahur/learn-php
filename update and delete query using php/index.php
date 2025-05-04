0431d9464f22972e24538a3bb7925a223f392ba7
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-04 16:06:25 +0600

    user update

---
+++ update and delete query using php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Change Account</h3>
+    <form action="includes/userUpdate.inc.php" method="post">
+        <input type="text" name="username" placeholder="User Name">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="email" name="email" placeholder="E-Mail">
+        <button type="submit">Update</button>
+    </form>
+
+    <h3>Delete Account</h3>
+    <form action="includes/userUpdate.inc.php" method="post">
+        <input type="text" name="username" placeholder="User Name">
+        <input type="password" name="pwd" placeholder="Password">
+        <button type="submit">Delete</button>
+    </form>
+</body>
+
+</html>
