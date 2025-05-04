16d7a0be52d76f3f0540eacc9896f4961474c5a5
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-04 16:15:50 +0600

    update & delete DB data using PHP

--- update and delete query using php/index.php
+++ update and delete query using php/index.php
@@ -17,7 +17,7 @@
     </form>
 
     <h3>Delete Account</h3>
-    <form action="includes/userUpdate.inc.php" method="post">
+    <form action="includes/userDelete.inc.php" method="post">
         <input type="text" name="username" placeholder="User Name">
         <input type="password" name="pwd" placeholder="Password">
         <button type="submit">Delete</button>
