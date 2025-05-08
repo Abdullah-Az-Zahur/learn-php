9b7317b94eeec6569c1f146a9eeb5f322774309b
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-08 11:03:50 +0600

    add : session config file

---
+++ login system exercise/includes/config_session.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+ini_set('session.cookie_httponly', 1);
+ini_set('session.use_only_cookies', 1);
+ini_set('session.use_strict_mode', 1);
+
+session_set_cookie_params([
+    'lifetime' => 1800, // 30 minutes
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Strict',
+]);
+
+session_start();
+
+if (!isset($_SESSION['last_regeneration'])) {
+    regenerate_session_id();
+} else {
+    $interval = 60 * 30; // 30 minutes
+    if (time() - $_SESSION['last_regeneration'] >= $interval) {
+        regenerate_session_id();
+    }
+}
+
+function regenerate_session_id()
+{
+    session_regenerate_id();
+    $_SESSION['last_regeneration'] = time();
+}
